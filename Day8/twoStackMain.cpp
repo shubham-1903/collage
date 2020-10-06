2cf0a63c2e081caea404bb17d6553e11781cc6b5
shubham-1903 <shubhamkr7677@gmail.com>
2020-10-06 19:45:49 +0530

    modified twoStackMain

--- Day8/twoStackMain.cpp
+++ Day8/twoStackMain.cpp
@@ -23,39 +23,41 @@ int main()
     switch (ch)
     {
     case 1:
-      if (s1.isFull1() && s2.isFull2())
-        cout << "Both Stack is full" << endl;
-      else
+      int x;
+      cout << "Enter item: ";
+      cin >> x;
+      if (x % 2 == 0)
       {
-
-        int x;
-        cout << "Enter item: ";
-        cin >> x;
         if (s1.isFull1())
           cout << "Stack 1 is full" << endl;
-        if (x % 2 == 0)
+        else
           s1.push1(x);
+      }
+      else
+      {
+        if (s2.isFull2())
+          cout << "Stack 2 is full" << endl;
         else
           s2.push2(x);
       }
       break;
     case 2:
-      if (s1.isEmpty1() && s2.isEmpty2())
-        cout << "Both stack is empty" << endl;
-
-      else if (s1.isEmpty1())
-        cout << "Stack 1 is empty:" << endl;
-      else if (s2.isEmpty2())
-        cout << "Stack 2 is empty:" << endl;
-      else
+      cout << "Enter 1 for pop() from stack 1" << endl;
+      cout << "Enter 2 for pop() from stack 2" << endl;
+      int y;
+      cin >> y;
+      if (y == 1)
       {
-        cout << "Enter 1 for pop() from stack 1" << endl;
-        cout << "Enter 2 for pop() from stack 2" << endl;
-        int y;
-        cin >> y;
-        if (y == 1)
+        if (s1.isEmpty1())
+          cout << "Stack 1 is empty:" << endl;
+        else
           cout << "POP1() " << s1.pop1() << endl;
-        else if (y == 2)
+      }
+      else if (y == 2)
+      {
+        if (s2.isEmpty2())
+          cout << "Stack 2 is empty:" << endl;
+        else
           cout << "POP2() " << s2.pop2() << endl;
       }
       break;
@@ -65,39 +67,34 @@ int main()
       cout << "Enter 2 for peak() from stack 2" << endl;
       int z;
       cin >> z;
-      if (z == 1 && s1.isEmpty1())
-        cout << "Stack 1 is empty:" << endl;
-      else
-        cout << "Peak1 value is " << s1.peak1() << endl;
-      if (z == 2 && s2.isEmpty2())
-        cout << "Stack 2 is empty:" << endl;
-      else
-        cout << "Peak2 value is " << s2.peak2() << endl;
+      if (z == 1)
+      {
+        if(s1.isEmpty1())  cout << "Stack 1 is empty:" << endl;
+        else cout << "Peak1 value is " << s1.peak1() << endl;
+      }
+      else if (z == 2)
+      {
+        if(s2.isEmpty2()) cout << "Stack 2 is empty:" << endl;
+        else cout << "Peak2 value is " << s2.peak2() << endl;
+      }
       break;
 
     case 4:
       if (s1.isFull1())
         cout << "Stack 1 is full" << endl;
-      else if (s2.isFull2())
-        cout << "Stack 1 is full" << endl;
-      else
+      if (s2.isFull2())
+        cout << "Stack 2 is full" << endl;
+      if (!s1.isFull1() && !s2.isFull2())
         cout << "Stack is not full" << endl;
       break;
 
     case 5:
-      // if (s1.isEmpty() && s2.isEmpty())
-      // {
-      //   cout << "Both stack is empty:" << endl;
-      //   // exit(0);
-      // }
-      // else if (s1.isEmpty())
-      //   cout << "Stack 1 is empty" << endl;
-      // else if (s2.isEmpty())
-      //   cout << "Stack 2 is empty" << endl;
-      // else
-      //   cout << "Stack is not empty" << endl;
-      cout << "s1: " << s1.isEmpty1() << endl;
-      cout << "s2: " << s2.isEmpty2() << endl;
+      if (s1.isEmpty1())
+        cout << "Stack 1 is empty" << endl;
+      if (s2.isEmpty2())
+        cout << "Stack 2 is empty" << endl;
+      if (!s1.isEmpty1() && !s2.isEmpty2())
+        cout << "Stack is not empty" << endl;
 
       break;
 
@@ -127,7 +124,8 @@ int main()
         }
         break;
       default:
-        continue;
+        cout << "Wrong choice." << endl;
+        break;
       }
       break;
 
